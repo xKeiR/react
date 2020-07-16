b246218ea0e3d757f5fad879f6e32f40037dc6fc
Kamil <Kamil@MacBook-Pro.local>
2020-07-16 22:14:07 +0200

    Updates

---
+++ precache-manifest.3b7db739aa1937e96378080e3477f36b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01819e8fe99c5357d119b1de3f95646",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "cb27cd792eccace5bf18",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e63e4cee8347e2e81be9",
+    "url": "/react/static/js/2.6cf6baa2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.6cf6baa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb27cd792eccace5bf18",
+    "url": "/react/static/js/main.2699468a.chunk.js"
+  },
+  {
+    "revision": "d609bb505eb8d5eb05c7",
+    "url": "/react/static/js/runtime-main.a9de1fd6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
